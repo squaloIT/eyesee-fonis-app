bf6bfb4c8f5bee5b1e07f1a6a742037c36f364d3
n.mihajlovic <n.mihajlovic@frontdeskanywhere.com>
2022-05-06 11:07:58 +0200

    feat: added theme to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-function App() {
-  return (
-    <div className="App">
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#f5f5f5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+    components: {
+      Button: {
+        variants: {
+          link: {
+            ":focus": {
+              outline: "none",
+              boxShadow: "none",
+            },
+          },
+        },
+      },
+    },
+  },
+});
 
-    </div>
-  );
-}
+const App = () => {
+  return <ChakraProvider theme={theme}>tmp</ChakraProvider>;
+};
 
 export default App;
